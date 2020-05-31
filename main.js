4a43559938dc8f78dfd997401b93048b342678a8
Erika Montijo <erikamontijo@erikas-mbp.attlocal.net>
2020-05-31 14:59:05 -0500

    i wrote the code and did the things

---
+++ main.js
@@ -0,0 +1,47 @@
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+
+
+console.log(`The Empire State Building has  ${empireStateBuilding.stories} stories`)
+console.log(`The Empire State Building is ${empireStateBuilding.height} feet high`)
+console.log(`The Empire State Building is ${empireStateBuilding.squareFeet} in square feet`)
+console.log(`The Empire State Building is ${empireStateBuilding.eastWestLength} from the east to the west of the building`)
+console.log(`The Empire State Building is ${empireStateBuilding.northSouthLength} from the north to the south of the building`)
+
+ var empireStateBuildingAddress = "350 Fifth Avenue, Manhattan, New York 10118";
+ 
+
+ var empireStateBuildingDate = 1931;
+ 
+
+ var empireStateBuildingCost = 40948900;
+ 
+
+ var empireStateBuildingOwner = "Empire State Realty Trust";
+
+ var empireStateBuildingArchitect = "Shreve, Lamb & Harmon"
+ 
+ 
+
+
+
+ console.log (empireStateBuildingArchitect)
+
+ console.log (empireStateBuilding["address"])
+ console.log (empireStateBuilding["constructionDate"])
+ console.log (empireStateBuilding["cost"])
+ console.log (empireStateBuilding["owner"])
+ console.log (empireStateBuilding["architect"])
+
+
+
